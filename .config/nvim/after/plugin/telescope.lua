66da07109cc600896c31ae2a0cd0fda9d8802df2
boared <>
2024-02-12 12:15:17 -0800

    Set keymap for Telescope live-grep with type filter

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -3,13 +3,17 @@ local ts_builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>fb", ts.extensions.file_browser.file_browser, {})
 vim.keymap.set("n", "<leader>ff", ts_builtin.find_files, {})
-vim.keymap.set("n", "<leader>lg", ts_builtin.live_grep, {})
-vim.keymap.set("n", "<leader>tg",
+vim.keymap.set("n", "<leader>lg",
     -- Live grep filtered by file type (mnemonic: type grep)
     -- Available types can be found by running `$ rg --type-list`
     function()
         local tf = vim.fn.input("File type > ")
-        ts_builtin.live_grep({ type_filter = tf })
+        if tf == nil or tf == "" then
+            tf = {}
+        else
+            tf = { type_filter = tf }
+        end
+        ts_builtin.live_grep(tf)
     end, {})
 vim.keymap.set("n", "<leader>ls", ts_builtin.buffers, {})
 
