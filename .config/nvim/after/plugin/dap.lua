28a54e7ebfe5da1f8545811e6a0de8cd69dc34f9
boared <>
2024-06-02 20:44:22 -0700

    Configure nvim-dap

--- .config/nvim/after/plugin/dap.lua
+++ .config/nvim/after/plugin/dap.lua
@@ -1,27 +1,78 @@
 local dap = require("dap")
-require("dapui").setup()
+local dapui = require("dapui")
 
--- C++ and Rust DAP
-dap.adapters.codelldb = {
-    type = "server",
-    port = "${port}",
-    executable = {
-        command = os.getenv("HOME") .. "/bin/codelldb/extension/adapter/codelldb",
-        args = { "--port", "${port}" },
-    }
+--------------------------------------------------------------------------------
+--                           Configure nvim-dap-ui
+--------------------------------------------------------------------------------
+dapui.setup()
+dap.listeners.after.event_initialized["dapui_config"]=function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated["dapui_config"]=function()
+  dapui.close()
+end
+dap.listeners.before.event_exited["dapui_config"]=function()
+  dapui.close()
+end
+
+--------------------------------------------------------------------------------
+--                             C/C++ and Rust DAP
+--
+-- Note: install LLVM to get access to this debug adapter
+-- https://lldb.llvm.org/resources/lldbdap.html
+--------------------------------------------------------------------------------
+dap.adapters.lldb = {
+    type = 'executable',
+    command = '/usr/bin/lldb-vscode',
+    name = 'lldb'
 }
-
-dap.configurations.rust = {
+dap.configurations.c = {
     {
-        name = "Launch file",
-        type = "codelldb",
-        request = "launch",
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
         program = function()
             return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
         end,
         cwd = '${workspaceFolder}',
         stopOnEntry = false,
+        args = {},
     },
 }
-dap.configurations.c = dap.configurations.rust
-dap.configurations.cpp = dap.configurations.rust
+dap.configurations.cpp = dap.configurations.c
+dap.configurations.rust = {
+    {
+        -- This is pretty much the same as dap.configurations.c but I couldn't find
+        -- a nice way to merge tables in Lua
+        name = 'Launch',
+        type = 'lldb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+
+        -- Rust specific configs
+        initCommands = function()
+            -- Find out where to look for the pretty printer Python module
+            local rustc_sysroot = vim.fn.trim(vim.fn.system('rustc --print sysroot'))
+
+            local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
+            local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
+
+            local commands = {}
+            local file = io.open(commands_file, 'r')
+            if file then
+                for line in file:lines() do
+                    table.insert(commands, line)
+                end
+                file:close()
+            end
+            table.insert(commands, 1, script_import)
+
+            return commands
+        end,
+    },
+}
